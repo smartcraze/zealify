4312e4eccd19413e7977ceadf9969617bd987c8e
WebDevHarsha <vasriharsha@gmail.com>
2024-02-16 19:29:22 +0530

    added next button

--- src/components/Main/Games.jsx
+++ src/components/Main/Games.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Games() {
+  const [number, setNumber] = useState(1);
+
+  const handlePrev = () => {
+    setNumber((prevNumber) => prevNumber - 1);
+  };
+
+  const handleNext = () => {
+    setNumber((prevNumber) => prevNumber + 1);
+  };
+
   return (
     <>
       <div className="App">
-      <video width="750" height="500" controls >
-      <source src="/public/level1/vid1.mp4" type="video/mp4"/>
-     </video>
+        <video width="750" height="500" controls>
+          <source src={`/public/level1/vid${number}.mp4`} type="video/mp4" />
+        </video>
+        <button onClick={handleNext}>Next</button>
+        <button onClick={handlePrev}>Previous</button>
       </div>
     </>
   );
