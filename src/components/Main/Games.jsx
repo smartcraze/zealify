4761f79748b7efc450ce4c2840f3d64603ce91fb
WebDevHarsha <vasriharsha@gmail.com>
2024-02-16 19:47:50 +0530

    video problem resolved

--- src/components/Main/Games.jsx
+++ src/components/Main/Games.jsx
@@ -1,18 +1,16 @@
-import React, { useState, useRef } from "react";
-
+import React, { useState } from "react";
 
 function Games() {
   const [number, setNumber] = useState(1);
-  const videoRef = useRef(null);
 
   const handlePrev = () => {
     if (number > 1) {
-      setNumber((prevNumber) => prevNumber - 1);
+      setNumber(number - 1);
     }
   };
 
   const handleNext = () => {
-    setNumber((prevNumber) => prevNumber + 1);
+    setNumber(number + 1);
   };
 
   const handleVideoEnded = () => {
@@ -23,19 +21,21 @@ function Games() {
     <>
       <div className="App">
         <video
-          ref={videoRef}
           width="750"
           height="500"
           controls
           onEnded={handleVideoEnded}
+          key={number} // Add a unique key to force reload the video element
         >
           <source
-            src={`/level1/vid${number}.mp4`}
+            src={`/public/level1/vid${number}.mp4`}
             type="video/mp4"
           />
         </video>
         <button onClick={handleNext}>Next</button>
-        <button onClick={handlePrev} disabled={number === 1}>Previous</button>
+        <button onClick={handlePrev} disabled={number === 1}>
+          Previous
+        </button>
       </div>
     </>
   );
