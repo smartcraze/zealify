bda28da3f2ea6c7899ae5a3b115c2edc57f89bf8
smartcraze <smartcraze.gmail.com>
2024-02-16 22:30:02 +0530

    logo

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -5,7 +5,9 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Zeal</Link>
+        <Link className="navbar-brand" to="/">
+          <img src="https://avatars.githubusercontent.com/u/133375708?v=4" style={{height:"50px", widows:"50px"}} alt="Logo" />
+        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
