f72d2556a0713030c5db6a64402aa40d6015191b
smartcraze <smartcraze.gmail.com>
2024-02-16 23:09:37 +0530

    added poster

--- src/components/Main/Gamecard.jsx
+++ src/components/Main/Gamecard.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
-import Games from './Games'
+import React from "react";
+import Games from "./Games";
 import { Link } from "react-router-dom";
 
 function Gamecard() {
   return (
     <>
-    {/* game1 */}
-    <div className="card" style={{ width: "18rem" }}>
-      <img src="..." className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        {/* Replaced <a> tag with <Link> component */}
-        <Link to="/play" className="btn btn-primary">Play Now </Link>
+      {/* game1 */}
+      <div className="container">
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="public\poster-game.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            {/* Replaced <a> tag with <Link> component */}
+            <Link to="/play" className="btn btn-primary">
+              Play Now{" "}
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
-
     </>
-  )
+  );
 }
 
-export default Gamecard
+export default Gamecard;
