211b1c783b83ee0b1d22bdabd33313e232b52e27
smartcraze <smartcraze.gmail.com>
2024-02-16 22:08:15 +0530

    added design

--- src/components/Main/Games.jsx
+++ src/components/Main/Games.jsx
@@ -32,58 +32,65 @@ function Games() {
 
   return (
     <>
-      <div className="App">
+      <div
+        className="App container bg-dark"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
         <video
           width="750"
           height="500"
           controls
-          // autoPlay
+          autoPlay
           onEnded={handleVideoEnded}
           key={number}
         >
-          <source
-            src={`/public/level1/vid${number}.mp4`}
-            type="video/mp4"
-          />
+          <source src={`/public/level1/vid${number}.mp4`} type="video/mp4" />
         </video>
-        <button
-          onClick={handlePrev}
-          disabled={currentQuestion === 1}
-          type="button"
-          className="btn btn-warning"
-        >
-          Previous
-        </button>
-        <button
-          onClick={handleNext}
-          type="button"
-          className="btn btn-success"
-        >
-          Next
-        </button>
+        <div className="m-5 p-5">
+          <button
+            onClick={handlePrev}
+            disabled={currentQuestion === 1}
+            type="button"
+            className="btn btn-warning"
+          >
+            Previous
+          </button>
+          <button
+            onClick={handleNext}
+            type="button"
+            className="btn btn-success"
+          >
+            Next
+          </button>
+        </div>
       </div>
-      <div>
+      <div className="">
         {(number === 3 || number === 6 || number === 9) && (
           <div>
-            <p>Question {currentQuestion}: are you gay?</p>
-            <button
-              onClick={handleTrue}
-              type="button"
-              className="btn btn-success"
-            >
-              True
-            </button>
-            <button
-              onClick={handleFalse}
-              type="button"
-              className="btn btn-danger"
-            >
-              False
-            </button>
+            <h1 style={{ textAlign: "center" }}>
+              Question {currentQuestion}: are you gay?
+            </h1>
+            <div className="d-flex justify-content-center">
+              <button
+                onClick={handleTrue}
+                type="button"
+                className="btn btn-success"
+              >
+                True
+              </button>
+              <button
+                onClick={handleFalse}
+                type="button"
+                className="btn btn-danger"
+              >
+                False
+              </button>
+            </div>
           </div>
         )}
-
-        <p>Your Score: {score}</p>
+        <div className="d-flex justify-content-center">
+          <h1 className="text-center">Your Score: {score}</h1>
+        </div>
       </div>
     </>
   );
