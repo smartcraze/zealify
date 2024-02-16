10672e84c9cb5e732a694148d2332c5657ddfe82
WebDevHarsha <vasriharsha@gmail.com>
2024-02-16 19:15:13 +0530

    video added

--- src/components/Main/Games.jsx
+++ src/components/Main/Games.jsx
@@ -3,18 +3,10 @@ import React from "react";
 function Games() {
   return (
     <>
-      <div className="card" style="width: 18rem;">
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
+      <div className="App">
+      <video width="750" height="500" controls >
+      <source src="/public/level1/vid1.mp4" type="video/mp4"/>
+     </video>
       </div>
     </>
   );
